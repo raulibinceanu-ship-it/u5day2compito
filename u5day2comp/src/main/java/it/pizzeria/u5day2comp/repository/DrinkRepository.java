6094edf3c92ffea6d1a4f139f2252208dd31fd51
raulibinceanu-ship-it <raulibinceanu@gmail.com>
2026-02-05 16:57:13 +0100

    Update DrinkRepository.java

--- u5day2comp/src/main/java/it/pizzeria/u5day2comp/repository/DrinkRepository.java
+++ u5day2comp/src/main/java/it/pizzeria/u5day2comp/repository/DrinkRepository.java
@@ -2,6 +2,13 @@ package it.pizzeria.u5day2comp.repository;
 
 import model.Drink;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DrinkRepository extends JpaRepository<Drink, Long> {
+
+    // QUERY CUSTOM JPQL
+    @Query("SELECT d FROM Drink d WHERE d.calories < :calories")
+    List<Drink> findLowCalorieDrinks(int calories);
 }
